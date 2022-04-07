f3ae18e035e1ebf07ca8a5a4d0001b4e8686f42e
shindo-develop <shindo.develop@gmail.com>
2022-04-07 20:56:40 +0900

    数字の分割 JavaScript

---
+++ 課題/No.177.数字の分割/answer.js
@@ -0,0 +1,14 @@
+function recursiveDigitsAdded(digits){
+  if(digits < 10)return digits;
+  return recursiveDigitsAddedHelper(digits, 0);
+}
+
+function recursiveDigitsAddedHelper(digits, total){
+  if(digits < 10)return total;
+  const digitsString = String(digits);
+  let carryOverDigits = 0
+  for(let i = 0; i < digitsString.length; i++){
+      carryOverDigits = carryOverDigits + Number(digitsString[i]);
+  };
+  return recursiveDigitsAddedHelper(carryOverDigits, carryOverDigits + total);
+}
