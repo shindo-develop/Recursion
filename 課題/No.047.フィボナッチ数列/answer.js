a70d4603370de5691993f11613d34887a273f6b0
shindo-develop <shindo.develop@gmail.com>
2022-04-22 20:43:04 +0900

    フィボナッチ数列 JavaScript

--- 課題/No.047.フィボナッチ数列/answer.js
+++ 課題/No.047.フィボナッチ数列/answer.js
@@ -1,20 +1,38 @@
+再帰
 function fibonacciHelper(fn1, fn2, count){
-  if(count < 1)return fn1;
-  return fibonacciHelper(fn2, fn1 + fn2, count - 1);
+    if(count < 1)return fn1;
+    return fibonacciHelper(fn2, fn1 + fn2, count - 1);
 }
 
 function fibonacci(n){
-  return fibonacciHelper(0, 1, n);
+    return fibonacciHelper(0, 1, n);
 }
 
-// for文版
-// function fibonacci(n){
-//   let fn1 = 0;
-//   let fn2 = 1;
-//   for(let i = 0; i < n; i++){
-//       let preFn1 = fn1
-//       fn1 = fn2;
-//       fn2 = preFn1 + fn2;
-//   }
-//   return fn1;
-// }
+// -------------------------------------------------------------------
+
+for文版
+function fibonacci(n){
+    let fn1 = 0;
+    let fn2 = 1;
+    for(let i = 0; i < n; i++){
+        let preFn1 = fn1
+        fn1 = fn2;
+        fn2 = preFn1 + fn2;
+    }
+    return fn1;
+}
+
+// -------------------------------------------------------------
+
+// while文版
+function fibonacci(n){
+  let fn1 = 0;
+  let fn2 = 1;
+  while(n >= 1){
+      let preFn1 = fn1;
+      fn1 = fn2;
+      fn2 = preFn1 + fn2;
+      n--;
+  };
+  return fn1;
+}
